3f65ad53a2d318a3153a6e0975d087517ad5ebed
dhchun1203 <dhchun1203@gmail.com>
2021-01-22 01:15:31 +0900

    React: component & props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import { React, Component } from "react";
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 import "./App.css";
 
-function App() {
-	return <div className="App">Hello, React!!</div>;
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Subject title="WEB" sub="world wide web!" />
+				<Subject title="React" sub="For UI" />
+				<TOC />
+				<Content title="HTML" desc="HTML is HyperText Markup" />
+			</div>
+		);
+	}
 }
 
 export default App;
