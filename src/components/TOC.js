4e231265f39aa6279831fb42f5f114e52397365a
dhchun1203 <dhchun1203@gmail.com>
2021-01-22 10:17:45 +0900

    React: state

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,19 +2,24 @@ import { Component } from "react";
 
 class TOC extends Component {
 	render() {
+		let list = [];
+		let data = this.props.data;
+		let i = 0;
+		while (i < data.length) {
+			list.push(
+				<li key={data[i].id}>
+					{/* 
+					이렇게 반복적으로 생성되는 컴포넌트에는 리액트가 
+					이해할 수 있게 고유한 key값을 넣어주는 것을 권장한다. 
+					*/}
+					<a href={"/content/" + data[i].id}>{data[i].title}</a>
+				</li>
+			);
+			i += 1;
+		}
 		return (
 			<nav>
-				<ul>
-					<li>
-						<a href="1.html">HTML</a>
-					</li>
-					<li>
-						<a href="2.html">CSS</a>
-					</li>
-					<li>
-						<a href="3.html">JavaScript</a>
-					</li>
-				</ul>
+				<ul>{list}</ul>
 			</nav>
 		);
 	}
