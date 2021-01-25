9757d609c986247ee57c9d6a96eb928ec32b488f
dhchun1203 <dhchun1203@gmail.com>
2021-01-26 08:10:18 +0900

    React: CreateContent

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -6,7 +6,25 @@ class CreateContent extends Component {
 		return (
 			<article>
 				<h2>Create</h2>
-				<form></form>
+				<form
+					action="/create_process"
+					method="post"
+					onSubmit={(e) => {
+						e.preventDefault();
+						// debugger;
+						this.props.onSubmit(e.target.title.value, e.target.desc.value);
+					}}
+				>
+					<p>
+						<input type="text" name="title" placeholder="title"></input>
+					</p>
+					<p>
+						<textarea name="desc" placeholder="description"></textarea>
+					</p>
+					<p>
+						<input type="submit"></input>
+					</p>
+				</form>
 			</article>
 		);
 	}
